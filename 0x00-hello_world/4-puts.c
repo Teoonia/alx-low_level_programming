fd0327a79b0dc0132be3f31ffea7b245c1ff64f8
Teoonia <olawooreayomikun@gmail.com>
2022-09-08 14:53:27 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main - entry point
+ * puts to replace the common printf
+ * return always (0) if successful
+ */
 int main(void)
 {
-puts("\"Programming is like a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
