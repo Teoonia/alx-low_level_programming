ad28b2276af4fdf4e2216f8f1f4d65b1815d4937
Teoonia <olawooreayomikun@gmail.com>
2022-09-08 15:28:45 -0700

    Task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 /**
- * main - entry point
- * puts to replace the common printf
- * return always (0) if successful
+ * main - Entry point
+ * void - empty
+ * puts - to replace the common printf
+ * Return - Always 0 (Success)
+ * (0) - means the code is successful
  */
 int main(void)
 {
