c2aff50f4f9701cfefdcea2a73a87a763be0d9f9
Teoonia <olawooreayomikun@gmail.com>
2022-09-09 19:16:06 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return (0) success
+ */
+int main(void)
+{
+char ch, e, q;
+e = 'e';
+q = 'q';
+for (ch = 'a' ; ch <= 'z' ; ch++);
+{
+if (ch != e && ch != q'\n')
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
