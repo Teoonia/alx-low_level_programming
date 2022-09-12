191701b819defc7015933a2e0b7d2badba147365
Teoonia <olawooreayomikun@gmail.com>
2022-09-12 02:37:09 -0700

    Task 8ch

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,10 +11,10 @@ for (x = 0; x < 10; x++)
 {
 putchar(x);
 }
-for (al = 10; al < 16; al++)
+for (al = 'a'; al <= 'f'; al++)
 {
-putchar(al)
+putchar(al);
 }
-putchar('\n')
-return (0)
+putchar('\n');
+return (0);
 }
