74915c21b0310b6baebea43615641f8ed7818c91
Teoonia <olawooreayomikun@gmail.com>
2022-09-12 02:33:54 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point.
+ * Return: 0 always success.
+ */
+int main(void)
+{
+int x;
+char al;
+for (x = 0; x < 10; x++)
+{
+putchar(x);
+}
+for (al = 10; al < 16; al++)
+{
+putchar(al)
+}
+putchar('\n')
+return (0)
+}
