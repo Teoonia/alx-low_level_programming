21f6c2185c8da9080fcb0e6b443dfc37bfef2f60
Teoonia <olawooreayomikun@gmail.com>
2022-09-12 02:59:52 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point.
+ * Return: 0 is success.
+ */
+int main(void)
+{
+char ch, e, q;
+e = 'e'
+q = 'q'
+for (ch = 'a'; ch = 'z'; ch++)
+{
+if (ch != 'e' && ch != 'q')
+puthar(ch);
+}
+putchar('\n');
+return (0);
+}
