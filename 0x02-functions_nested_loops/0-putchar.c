dfa8ed891437d57e02ba19c6fadaafdb31610e8c
Teoonia <olawooreayomikun@gmail.com>
2022-09-14 06:15:22 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.h";
+#include "main.h"
 /**
  * main - print putchar
  * Return: always 0
@@ -6,14 +6,12 @@
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
+char *sh = "_putchar";
+while (*sh)
+{
+_putchar(*sh);
+sh++;
+}
 _putchar('\n');
 return (0);
 }
