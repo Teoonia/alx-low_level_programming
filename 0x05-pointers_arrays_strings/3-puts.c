0d4c39b382fc9ea5cc4d4e307cf44f63a7adbc31
Teoonia <olawooreayomikun@gmail.com>
2022-09-21 07:27:51 -0700

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+nclude "main.h"
+/**
+ * _puts - a function that prints a string
+ * @str: string input
+ * Return: string
+ */
+void _puts(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+	_Putchar(str[i]);
+}
+_putchar('\n')
+}
