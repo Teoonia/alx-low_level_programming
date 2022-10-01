130e8e00e7ac2bc0a4e4a2952273c062f0cb278b
Teoonia <olawooreayomikun@gmail.com>
2022-10-01 13:42:10 -0700

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  *
  * Return: Always o.
  */
-int main(int __attribute__((__unused__))int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 printf("%s \n", argv[0]);
 return(0);
