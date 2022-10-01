e3b448070c97740bd72ce3c25a31b43f63c6d14d
Teoonia <olawooreayomikun@gmail.com>
2022-10-01 13:59:42 -0700

    Task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,4 +1,10 @@
+/*
+ * File: 2-args.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <stdio.h>
+
 /**
  * main - Prints all arguments it receives.
  * @argc: The number of arguments supplied to the program.
@@ -6,9 +12,12 @@
  *
  * Return: Always 0.
  */
-int main(int argc, char *argv[]);
+int main(int argc, char *argv[])
 {
-while(argc--)
-printf("%s \n", *argv++);
-return(0);
+	int arg;
+
+	for (arg = 0; arg < argc; arg++)
+		printf("%s\n", argv[arg]);
+
+	return (0);
 }
