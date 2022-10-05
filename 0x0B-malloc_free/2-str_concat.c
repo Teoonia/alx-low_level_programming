a33d9e57ab5f832b681fbe380617f62ffa5ef4d4
Teoonia <olawooreayomikun@gmail.com>
2022-10-05 09:18:51 -0700

    Task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "main.h"
+
+char *str_concat(char *s1, char *s2)
+{
+char *strconc;
+int strcount1 strcount2
+
+for (strcount1 = 0; s1[strcount1]; strcount++)
+for (strcount2 = 0; s2[strcount2]; strcount++)
+strconc = malloc((sizeof(char) + strcount1 +strcount2) + 1)
+
+return (strconc);
+}
