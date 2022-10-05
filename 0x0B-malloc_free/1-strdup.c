8ddc42b35a4a3ebe7b9a14477cbf149bf5cf1d7f
Teoonia <olawooreayomikun@gmail.com>
2022-10-05 08:26:21 -0700

    Task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "main.h"
+
+char *_strdup(char *str)
+{
+char *dup;
+int str_lent;
+int count;
+
+for (str_lent = 0; str[str_lent] != '\0'; str_lent++);
+dup = malloc((sizeof(char) * str_lent) + 1);
+for (count = 0; str[count] != '\0'; count++)
+	str[count] = dup[count];
+dup[count] = '\0';
+return (dup);
+}
